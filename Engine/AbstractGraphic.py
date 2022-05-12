4d73ad2d2f9e2dc0380d260978389d5cdf672a44
Clément <klement.bzh@gmail.com>
2022-05-12 11:40:43 +0200

    deleted useless files

--- Engine/AbstractGraphic.py
+++
@@ -1,8 +0,0 @@
-class AbstractGraphic():
-    def __init__(self): 
-        self.width = 0
-        self.height = 0
-        self._graphic = None
-    
-    def getGraphic(self):
-        return self._graphic
